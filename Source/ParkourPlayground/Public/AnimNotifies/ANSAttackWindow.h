dd2bf802a306a105c3269e28844e064921285f28
Patryk Puszko <ppuszko3@gmail.com>
2025-05-11 21:48:33 +0200

    Refined Attack Logic

---
+++ Source/ParkourPlayground/Public/AnimNotifies/ANSAttackWindow.h
@@ -0,0 +1,17 @@
+
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimNotifies/AnimNotifyState.h"
+#include "ANSAttackWindow.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PARKOURPLAYGROUND_API UANSAttackWindow : public UAnimNotifyState
+{
+	GENERATED_BODY()
+	
+};

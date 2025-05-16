f2b13ec98d883b8f0a4bbbcd8e0091357f99c460
Patryk Puszko <ppuszko3@gmail.com>
2025-05-16 12:53:55 +0200

    Tweak Anims Code Sync

---
+++ Source/ParkourPlayground/Private/AnimNotifies/ANSToggleWeaponCollider.cpp
@@ -0,0 +1,5 @@
+
+
+
+#include "AnimNotifies/ANSToggleWeaponCollider.h"
+
---
+++ Source/ParkourPlayground/Public/AnimNotifies/ANSToggleWeaponCollider.h
@@ -0,0 +1,17 @@
+
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimNotifies/AnimNotifyState.h"
+#include "ANSToggleWeaponCollider.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PARKOURPLAYGROUND_API UANSToggleWeaponCollider : public UAnimNotifyState
+{
+	GENERATED_BODY()
+	
+};
